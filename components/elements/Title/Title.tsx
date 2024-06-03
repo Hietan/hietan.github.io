6102a8c55e251517e2a1631c2d7230fa4b204487
Hietan <hietan.code@gmail.com>
2024-06-03 14:39:08 +0900

    Add TypeScript Types

--- components/elements/Title/Title.tsx
+++ components/elements/Title/Title.tsx
@@ -1,6 +1,11 @@
+import type { ReactNode } from 'react';
 import styles from "./Title.module.css"
 
-export default function Title(props) {
+type Props = {
+	children?: ReactNode;
+}
+
+export default function Title(props: Props) {
 	return (
 		<h2 className={styles.title}>{props.children}</h2>
 	);
