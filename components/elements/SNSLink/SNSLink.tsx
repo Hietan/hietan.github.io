c3600403fbb9d5f45d2fbba8d214803842e20bc4
Hietan <hietan622@gmail.com>
2024-07-26 17:27:11 +0900

    add SNSLink

---
+++ components/elements/SNSLink/SNSLink.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from "./SNSLink.module.scss"
+
+type Props = {
+	href: string,
+	img: string,
+	serviceName: string,
+	accountName: string
+}
+
+export default function SNSLink(props: Props) {
+	return (
+		<Link
+			className={styles.wrapper}
+			href={props.href}
+			target="_blank"
+		>
+			<div className={styles.imageWrapper}>
+				<Image
+					className={styles.image}
+					src={props.img}
+					width={48}
+					height={48}
+					alt={`Logo of ${props.serviceName}`}
+				/>
+			</div>
+			<div className={styles.textWrapper}>
+				<div className={styles.serviceName}>
+					{props.serviceName}
+				</div>
+				<div className={styles.accountName}>
+					<span className={styles.at}>@</span>
+					{props.accountName}
+				</div>
+			</div>
+		</Link>
+	);
+}
