e9e184a06d89ae9509e358ec8b12051e0fe5694f
Hietan <yamasaki.kazuma.yj9@naist.ac.jp>
2025-10-25 17:40:41 +0900

    feat: add Carbon style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@carbon/styles/css/styles.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        data-carbon-theme="g10"
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
