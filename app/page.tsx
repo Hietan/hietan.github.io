098bb747dce521fda0e0197d4fe1e49ce2949ffe
Hietan <yamasaki.kazuma.yj9@naist.ac.jp>
2025-11-11 14:15:49 +0900

    feat: add contents

--- app/page.tsx
+++ app/page.tsx
@@ -5,63 +5,61 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
         <Tile>
-          <h2 className="cds--type-productive-heading-02">Education</h2>
+          <h2 className="cds--type-productive-heading-02" style={{marginBottom: "1rem"}}>Profile Information</h2>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableHeader>Name</TableHeader>
+                <TableCell>Kazuma Yamasaki (山﨑 和真)</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableHeader>Affiliation</TableHeader>
+                <TableCell>Nara Institute of Science and Technology (NAIST), Japan</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableHeader>Laboratory</TableHeader>
+                <TableCell>Software Engineering Laboratory</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableHeader>Degree</TableHeader>
+                <TableCell>Ph.D. (Year1)</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableHeader>Email</TableHeader>
+                <TableCell>yamasaki.kazuma.yj9@naist.ac.jp</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Tile>
+        <Tile>
+          <h2 className="cds--type-productive-heading-02" style={{marginBottom: "1rem"}}>Research Interest</h2>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableHeader>Research Area</TableHeader>
+                <TableCell>Software Engineering, Programming Language, Computer Science</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableHeader>Keywords</TableHeader>
+                <TableCell>AI (Artificial Intelligence), LLM (Large Language Model), Software Library, Breaking Change</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableHeader>Research Topics</TableHeader>
+                <TableCell>
+                  <ul>
+                    <li>AI oriented Programming Language (AIoPL)</li>
+                    <li>Breaking Change Detection using Dynamic Analysis</li>
+                    <li>Vulnerability Detection using LLMs</li>
+                    <li>Analyzing Migration Guides in Software Libraries</li>
+                  </ul>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Tile>
+        <Tile>
+          <h2 className="cds--type-productive-heading-02" style={{marginBottom: "1rem"}}>Education</h2>
           <Table>
             <TableHead>
               <TableRow>
@@ -72,17 +70,17 @@ export default function Home() {
             </TableHead>
             <TableBody>
               <TableRow>
-                <TableCell>2020/04 – 2024/03</TableCell>
+                <TableCell>2020-04 ~ 2024-03</TableCell>
                 <TableCell>Bachelor</TableCell>
                 <TableCell>Doshisha University</TableCell>
               </TableRow>
               <TableRow>
-                <TableCell>2024/04 – 2025/09</TableCell>
+                <TableCell>2024-04 ~ 2025-09</TableCell>
                 <TableCell>Master</TableCell>
                 <TableCell>Nara Institute of Science and Technology (NAIST)</TableCell>
               </TableRow>
               <TableRow>
-                <TableCell>2025/09 – Present</TableCell>
+                <TableCell>2025-09 ~ Present</TableCell>
                 <TableCell>Doctor</TableCell>
                 <TableCell>Nara Institute of Science and Technology (NAIST)</TableCell>
               </TableRow>
